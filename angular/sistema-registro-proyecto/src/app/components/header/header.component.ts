ac8b7dde9f8f8e05dd3939be2344041bd9fb5014
JhodanSebastianCorredorAcevedoADSO <corredorjhodan@gmail.com>
2024-02-15 07:25:51 -0500

    cambios

--- angular/sistema-registro-proyecto/src/app/components/header/header.component.ts
+++ angular/sistema-registro-proyecto/src/app/components/header/header.component.ts
@@ -7,7 +7,6 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-header',
   standalone: true,
-  // se hacen importaciones de los distinbtos moduelos
   imports: [RouterLink, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
